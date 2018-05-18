e86765c422be8a00f8de4276023c3c118331d5b4
Dongliang Mu <mudongliangabcd@gmail.com>
2018-05-18 14:00:54 -0400

    add one tool to generate dockerfile

---
+++ tools/gen_dockerfile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from __future__ import print_function
+
+def main:
+    print("Read config file and generate one Dockerfile")
+
+if __name__ == '__main__':
+    main()
